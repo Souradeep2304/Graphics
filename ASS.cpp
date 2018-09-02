2b1f908c059152d541d4f3c3b0d630d80a628901
Souradeep Banerjee <souradeepbanerjee2304@gmail.com>
2018-09-02 11:44:28 +0530

    Code updated

--- ASS.cpp
+++ ASS.cpp
@@ -29,9 +29,9 @@ void display(int x0, int y0, int x1, int y1,int a, int b, int c)
    float x=x0,y=y0;
    float dy=(float)(abs(y1)-abs(y0));
    float dx=(float)(abs(x1)-abs(x0));
-   //float dy=(float)((y1)-(y0));
-   //float dx=(float)((x1)-(x0));
- 
+
+if(dy<dx)
+{
    float p=2*dy-dx;
  if(x1>x)
 {
@@ -141,6 +141,123 @@ while(x!=x1)
 }
 }
 
+if(dy>dx)
+{
+   float p=2*dx-dy;
+ if(x1>x)
+{
+  if(y1>y)
+{
+    while(y<=y1)
+    {
+        if(p>=0)
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            x=x+1;
+            p=p+2*dy+2*dx;
+        }
+        else
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            p=p+2*dy;
+        }
+        y=y+1;
+    }
+}
+if(y1<y)
+{
+while(y!=y1)
+    {
+        if(p>=0)
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            x=x+1;
+            p=p+2*dy+2*dx;
+        }
+        else
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            p=p+2*dy;
+        }
+        y=y-1;
+    }
+}
+	
+
+}
+ if(x1<x)
+{
+  if(y1>y)
+{
+    while(y<=y1)
+    {
+        if(p>=0)
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            x=x-1;
+            p=p+2*dy+2*dx;
+        }
+        else
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            p=p+2*dy;
+        }
+        y=y+1;
+    }
+}
+if(y1<y)
+{
+while(y!=y1)
+    {
+        if(p>=0)
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            x=x-1;
+            p=p+2*dy+2*dx;
+        }
+        else
+        {
+            glBegin(GL_POINTS);
+	    glColor3f(a,b,c);
+	    glVertex2f(x,y);
+	    glEnd();
+            p=p+2*dy;
+        }
+        y=y-1;
+    }
+}
+	
+
+}
+}
+
+
+
+
+
+}
+
 
 void name(int x, int y, char String[], int a, int b, int c)
 {
@@ -171,19 +288,19 @@ void draw(void)
 
     	name(-100,-99,string1,1,0,0);
 	name(-100,-90,string2,0,1,0);
-	name(-100,-80,string3,0,0,1);
-	name(-100,-70,string4,0,1,1);
+	name(-100,-80,string3,255,165,0);
+	name(-100,-70,string4,0,0,1);
 	name(5,95,string5,1,1,1);
 	name(95,5,string6,1,1,1);
 
 	display(10,10,40,30,1,0,0);
 	display(10,30,50,90,1,0,0);
-	display(-10,30,-20,60,0,0,1);
-	display(-10,10,-50,20,0,0,1);
+	display(-10,30,-20,60,255,165,0);
+	display(-10,10,-50,20,255,165,0);
 	display(-20,-10,-35,-20,0,1,0);
 	display(-10,-40,-30,-70,0,1,0);
-	display(10,-10,25,-55,0,1,1);
-	display(35,-25,55,-30,0,1,1);
+	display(10,-10,25,-55,0,0,1);
+	display(35,-25,55,-30,0,0,1);
 	
 
 	
