1c968e863d69b2d9cd736e9bcdea17c6bdd2d8d7
Souradeep Banerjee <souradeepbanerjee2304@gmail.com>
2018-09-04 02:31:43 +0530

    Code updated acc to mouse clicks

--- ASS.cpp
+++ ASS.cpp
@@ -4,12 +4,14 @@
 #include <GL/glu.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h> 
+GLint a,b,c,d;
 void yaxis()
 {
 glColor3f(1,1,1);
 glBegin(GL_LINE_STRIP);
-glVertex2f(0,-100);
-glVertex2f(0,100);
+glVertex2f(320,0);
+glVertex2f(320,480);
 glEnd();
 
 }
@@ -17,246 +19,98 @@ void xaxis()
 {
 glColor3f(1,1,1);
 glBegin(GL_LINE_STRIP);
-glVertex2f(100,0);
-glVertex2f(-100,0);
+glVertex2f(640,240);
+glVertex2f(0,240);
 glEnd();
 
 }
-
+  void putPixel (int x, int y,int a,int b,int c)  
+ {  
+   glColor3f (a,b,c); 
+   glBegin (GL_POINTS);  
+   glVertex2i (x, y);   
+   glEnd ();  
+   glFlush ();  
+  } 
 
 void display(int x0, int y0, int x1, int y1,int a, int b, int c)
 {
-   float x=x0,y=y0;
-   float dy=(float)(abs(y1)-abs(y0));
-   float dx=(float)(abs(x1)-abs(x0));
+   int dx = abs (x1 - x0);  
+  int dy = abs (y1 - y0);  
+  int x, y;  
+  if (dx >= dy)  
+  {  
+   int d = 2*dy-dx;  
+   int ds = 2*dy;  
+   int dt = 2*(dy-dx);  
+       if (x0 < x1)   
+      {  
+            x = x0;  
+            y = y0;  
+       }  
+       else  
+        {   
+             x = x1;  
+            y = y1;  
+             x1 = x0;  
+            y1 = y0;  
+        }  
+      putPixel (x, y,a,b,c);  
+   while (x < x1)  
+  {  
+        if (d < 0)  
+        d += ds;  
+       else {  
+            if (y < y1) {  
+              y++;  
+             d += dt;  
+             }   
+            else {  
+              y--;  
+           d += dt;  
+          }  
+            }  
+   x++;  
+       putPixel (x, y,a,b,c);  
+      }  
+       }  
+       else {   
+            int d = 2*dx-dy;  
+             int ds = 2*dx;  
+             int dt = 2*(dx-dy);  
+            if (y0 < y1) {  
+           x = x0;  
+           y = y0;  
+             }  
+             else {   
+           x = x1;  
+             y = y1;  
+             y1 = y0;  
+             x1 = x0;  
+            }  
+            putPixel (x, y,a,b,c);   
+       while (y < y1)  
+        {  
+             if (d < 0)  
+                 d += ds;  
+             else {  
+                     if (x > x1){  
+                     x--;  
+                      d += dt;  
+                   } else {  
+                     x++;  
+                     d += dt;  
+                   }  
+              }  
+              y++;  
+              putPixel (x, y,a,b,c);  
+       }  
+       }  
+ 
 
-if(dy<dx)
-{
-   float p=2*dy-dx;
- if(x1>x)
-{
-  if(y1>y)
-{
-    while(x<=x1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            y=y+1;
-            p=p+2*dy-2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        x=x+1;
-    }
-}
-if(y1<y)
-{
-while(x<=x1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            y=y-1;
-            p=p+2*dy-2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        x=x+1;
-    }
-}
-	
-
-}
- if(x1<x)
-{
-  if(y1>y)
-{
-    while(x!=x1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            y=y+1;
-            p=p+2*dy-2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        x=x-1;
-    }
-}
-if(y1<y)
-{
-while(x!=x1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            y=y-1;
-            p=p+2*dy-2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        x=x-1;
-    }
-}
-	
-
-}
-}
-
-if(dy>dx)
-{
-   float p=2*dx-dy;
- if(x1>x)
-{
-  if(y1>y)
-{
-    while(y<=y1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            x=x+1;
-            p=p+2*dy+2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        y=y+1;
-    }
-}
-if(y1<y)
-{
-while(y!=y1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            x=x+1;
-            p=p+2*dy+2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        y=y-1;
-    }
-}
-	
-
-}
- if(x1<x)
-{
-  if(y1>y)
-{
-    while(y<=y1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            x=x-1;
-            p=p+2*dy+2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        y=y+1;
-    }
-}
-if(y1<y)
-{
-while(y!=y1)
-    {
-        if(p>=0)
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            x=x-1;
-            p=p+2*dy+2*dx;
-        }
-        else
-        {
-            glBegin(GL_POINTS);
-	    glColor3f(a,b,c);
-	    glVertex2f(x,y);
-	    glEnd();
-            p=p+2*dy;
-        }
-        y=y-1;
-    }
-}
-	
-
-}
 }
 
 
-
-
-
-}
 
 
 void name(int x, int y, char String[], int a, int b, int c)
@@ -270,44 +124,56 @@ void name(int x, int y, char String[], int a, int b, int c)
     }
 }
 
+
+void mouse(int button, int state, int mx, int my)
+{
+
+
+if(button == GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+
+{
+a=mx;
+b=480-my;
+}
+
+
+else if(button == GLUT_LEFT_BUTTON && state==GLUT_UP)
+{
+c=mx;
+d=480-my;
+display(a,b,c,d,0,0,1);
+printf("%d %d %d %d",a,b,c,d);
+
+glFlush();
+}
+}
+
+
+
 void draw(void)
 {
-	static int a=100;
 	glClearColor(0, 0, 0,0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glMatrixMode(GL_PROJECTION);
+	
 	glLoadIdentity();
-	gluOrtho2D(-100.0, 100.0, -100.0,100.0);
-	char string1[64]="Shivansh Thapliyal";
-	char string2[64]="Souradeep Banerjee";
-	char string3[64]="Shishir Goyal";
-	char string4[64]="Siddharth Sharma";
-	char string5[64]="X";
-	char string6[64]="Y";
-
-    	name(-100,-99,string1,1,0,0);
-	name(-100,-90,string2,0,1,0);
-	name(-100,-80,string3,255,165,0);
-	name(-100,-70,string4,0,0,1);
-	name(5,95,string5,1,1,1);
-	name(95,5,string6,1,1,1);
-
-	display(10,10,40,30,1,0,0);
-	display(10,30,50,90,1,0,0);
-	display(-10,30,-20,60,255,165,0);
-	display(-10,10,-50,20,255,165,0);
-	display(-20,-10,-35,-20,0,1,0);
-	display(-10,-40,-30,-70,0,1,0);
-	display(10,-10,25,-55,0,0,1);
-	display(35,-25,55,-30,0,0,1);
-	
-
-	
-	
+	gluOrtho2D(0.0f, 640.0f, 0.0f,480.0f);
 	xaxis();
 	yaxis();
-	glFlush();
+	
+char string1[64]="Shivansh Thapliyal";
+char string2[64]="Souradeep Banerjee";
+char string3[64]="Shishir Goyal";
+char string4[64]="Siddharth Sharma";
+char string5[64]="X";
+char string6[64]="Y";
+name(0.0f,25.0f,string1,1,0,0);
+	name(0.0f,50.0f,string2,0,1,0);
+	name(0.0f,75.0f,string3,255,165,0);
+	name(0.0f,100.0f,string4,0,0,1);
+	//name(5,475,string5,1,1,1);
+	//name(475,5,string6,1,1,1);
+glFlush();
 
 }
 
@@ -316,11 +182,12 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(30, 50);
-	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(200,200);
+	glutInitWindowSize(640,480);
 	glutCreateWindow("Assignment");
- 	
-	glutDisplayFunc(draw);
+        glutDisplayFunc(draw);
+	glutMouseFunc(mouse);
+//init();
 	glutMainLoop();
 	return 0;
 }
